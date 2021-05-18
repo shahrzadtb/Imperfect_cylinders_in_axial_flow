08f9358ea0aa82deea1c73ce6e76ca9486483433
shahrzadtb <78036346+shahrzadtb@users.noreply.github.com>
2021-05-18 13:34:03 -0400

    Update Call_static_stab.m

--- static_and_dynamic_codes/Call_static_stab.m
+++ static_and_dynamic_codes/Call_static_stab.m
@@ -1,4 +1,4 @@
-%This is the main file
+%This is the main file 
 %This code uses an eigenvalue problem solver to find the critical flow velocity for the divergence of a flexible cylinder in contact with axial flow
 %The cylinder may have an imperfect upstream support and initial inclination with respect to the oncoming flow
 %Developed by: Mojtaba Kheiri and Shahrzad Tabatabaei
@@ -16,42 +16,45 @@ global stvar
 
 %%%%%%%%%%%%%%%%%%%   System parameters   %%%%%%%%%%%%%%%%%%%
 
-stvar.betta = 0.5 ;   %mass ratio
-stvar.gamma = 17.6 ;     %gravity effect
-stvar.khi_e = 0.00792 ;
-stvar.khi_e_bar = 0.01056 ; 
-stvar.epsilon_cn = 25.3 * 0.0100 ;
-stvar.epsilon_ct = 25.3 * 0.0125 ;
-stvar.xi = 1.22 ; % confinement parameter
-stvar.alpha_star = 0.0 ; % viscoelastic damping
-stvar.mui_star = 0.0 ; % hysteretic damping
-stvar.h = 0.455 ; % diameter of cylinder/Dh
+stvar.betta = 0.5 ; %mass ratio
+stvar.gamma = 17.6 ; %dimensionless gravitational parameter
+stvar.khi_e = 0.00792 ;%end-piece shape factor 2
+stvar.khi_e_bar = 0.01056 ; %end-piece shape factor 3
+epsil = 25.3; %slenderness ratio
+cn = 0.001; %normal frictional drag coefficient
+ct = 0.0125; %tangential frictional drag coefficient
+stvar.epsilon_cn = epsil * cn ; 
+stvar.epsilon_ct = epsil * ct ;
+stvar.xi = 1.22 ; %confinment parameter 1
+stvar.alpha_star = 0.0 ; %viscoelastic damping
+stvar.mui_star = 0.0 ; %hysteretic damping
+stvar.h = 0.455 ; %confinment parameter 2
 stvar.epsilon_c = 0.0 ; %viscous forces effect at zero flow velocity   
 stvar.f = 0.8 ; %fairly streamlined
-stvar.cb = 0.1 ;
-stvar.k0_star = 10 ^ 10 ;
-stvar.k0 = 10 ^ 10 ;
+stvar.cb = 0.1 ; %end-piece form drag coefficient
+stvar.k0_star = 10 ^ 10 ; %dimensionless rotational spring constant
+stvar.k0 = 10 ^ 10 ; %dimensionless translational spring constant
+stvar.theta_knot = ( 5 * pi ) / 180 ; %initial inclination angle
 
-%Varying parameters
-u = 0.001 : 0.01 : 20.0; %flow velocity range
-stvar.N = 10; %number of eignmodes
-stvar.typ = 6 ; %type of the configuration
+%%%%%%%%%%%%%%%%%%%   varying parameters   %%%%%%%%%%%%%%%%%%%
+
+
+u = 0.001 : 0.01 : 20.0;    %nondimensional flow velocity
+stvar.N = 10;    %number of eignmodes
+stvar.N_S = 35 ;     %number of elements used in the FDM solution
 
 stvar.fformat = 1; %txt file output in plotter
 stvar.m = 5; %number of modes considered in Tecplot output
-
-
-% Static equation values %
-
-stvar.N_S = 35 ;  % number of nodes on the beam
-stvar.theta_knot = ( 5 * pi ) / 180 ;   % initial inclination angle
 
 
 
 figure ( 1 );
 omegamat = zeros ( 2 * stvar.N , length ( u ) );
 
-%calling LinCylinStaticFullEqu.m
+
+%%%%%%%%%%%%%%%%%%%   calling static_stab   %%%%%%%%%%%%%%%%%%%
+
+
 for i = 1 : length ( u )
     omega = static_stab ( stvar.typ , stvar.N , u ( i ) );
     omegamat ( : , i ) = omega;
@@ -60,7 +63,11 @@ for i = 1 : length ( u )
 end
 
 grid on
-%prepare for tecplot
+
+
+%%%%%%%%%%%%%%%%%%%   prepare for Tecplot   %%%%%%%%%%%%%%%%%%%
+
+
 Pstp = u;
 plotter;
 
